ece0de54b33af41c4a6fa2dcbce5a87c87e653fc
Navaneeth Joy <navaneethjoy04@gmail.com>
2026-08-03 15:54:01 +0530

    think commit

---
+++ chapter1/loopt.js
@@ -0,0 +1,12 @@
+let cars = ["tesla","ferrari"]
+let i=0
+let length = cars.length
+while(i<length){
+    console.log('the value at',i ,"is ", cars[i])
+    i++
+}
+//
+let companies = ["xioami","apple"]
+for(let i=0;i<companies.length;i++){
+    console.log('this company',companies[i], 'is at',i)
+}
