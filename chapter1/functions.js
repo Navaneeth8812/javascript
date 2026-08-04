4e07ab0f4626bac23ade76419aea384d52b19ecf
Navaneeth Joy <navaneethjoy04@gmail.com>
2026-08-05 03:35:48 +0530

    changed

--- chapter1/functions.js
+++ chapter1/functions.js
@@ -1,5 +1,5 @@
 function myFunction(arg1,arg2){
-    console.log(arg1!=arg2)
+    console.log(arg1!=arg2)  //==, === also can be used
 }
 myFunction(2,3)
 myFunction(2,44)
--- chapter1/variab.js
+++ chapter1/variab.js
@@ -1,3 +1,2 @@
 let myName = "navaneeth" //hey this is a comment
 console.log(myName)
-
