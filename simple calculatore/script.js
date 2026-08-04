e36fd76ca7f9505ac944911f35a17711abede8bf
Navaneeth Joy <navaneethjoy04@gmail.com>
2026-08-05 03:52:03 +0530

    chap

---
+++ simple calculatore/script.js
@@ -0,0 +1,16 @@
+function printNum1(){
+    document.getElementById('out').innerHTML=1;
+}
+function printNum2(){
+    document.getElementById('out').innerHTML=2;
+}
+function printAdd(){
+    document.getElementById('out').innerHTML='+';
+}
+function printSub(){
+    document.getElementById('out').innerHTML='-';
+}
+function printDiv(){
+    document.getElementById('out').innerHTML='/';
+}
+
